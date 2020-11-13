69b1e8782bc45f6a8e7f0e55286b9f6b9a7cc6b2
gold999-lgtm <72864295+gold999-lgtm@users.noreply.github.com>
2020-11-13 15:53:56 +0530

    Add files via upload

---
+++ wifi code.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 17 17:00:16 2020
+
+@author: anuj
+"""
+import subprocess
+data=subprocess.check_output(["netsh","wlan","show","profiles"]).decode("utf-8").split("\n")
+profiles=[i.split(":")[1][1:-1]
+          for i in data if "All User Profile" in i]
+for i in profiles:
+    results=subprocess.check_output(["netsh","wlan","show","profile",i,"key=clear"]).decode("utf-8").split("\n")
+    results=[b.split(":")[1][1:-1] for b in results if "Key Content"in b]
+    try:
+        print("{:<30}|{:<}".format(i,results[0]))
+    except IndexError:
+        print("{:<30}|{:<}".format(i,""))
+       
+        
