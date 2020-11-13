56b3507a0f3210b06d3c7d0551b4f603ae932f49
gold999-lgtm <72864295+gold999-lgtm@users.noreply.github.com>
2020-11-13 15:53:24 +0530

    Add files via upload

---
+++ whats app code.py
@@ -0,0 +1,7 @@
+import pywhatkit
+x=["+918076008958","+917428032704","+918595827141"]
+for i in x:
+    pywhatkit.sendwhatmsg(i,"Hello  I am sending this msg from Anaconda Spyder",16,15)
+
+
+pywhatkit.info("andromeda",lines=10)
