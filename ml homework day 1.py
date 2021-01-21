7204ec777e29abbf0d55f6b65380e27b44a8ba58
gold999-lgtm <72864295+gold999-lgtm@users.noreply.github.com>
2021-01-21 18:10:03 +0530

    Delete ml homework day 1.py

--- ml homework day 1.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-df=pd.read_csv("Salaries.csv")
-data_gender=df["sex"].value_counts().reset_index()
-data_gender_ref=pd.DataFrame()
-data_gender_ref["Male"]=[data_gender["sex"][0]]
-data_gender_ref["Female"]=[data_gender["sex"][1]]
-plt.pie([data_gender_ref["Male"],data_gender_ref["Female"]],explode=[0,0],labels=["male","female"],autopct="%1.1f%%")
-plt.axis("equal")
